777c558b703434013e00ed003fe83e2dbf67f259
MeryM13 <globa2003marya@gmail.com>
2024-09-09 11:44:26 +0700

    * added Utility folder for stores and styles * rewrote MainWindow to MainView (w/o tabcontrol items), added MainViewModel, set bindings

---
+++ WeatherAnalitycs/Utility/SearchParamsStore.cs
@@ -0,0 +1,17 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherDataParser.CLASSES;
+
+namespace WeatherAnalitycs.Utility
+{
+    internal class SearchParamsStore:BaseViewModel
+    {
+        public int StationId { get; set; } = 0;
+        public DateTime From { get; set; } = new(2012, 01, 01);
+        public DateTime To { get; set; } = DateTime.Now;
+    }
+}
---
+++ WeatherAnalitycs/ViewModel/MainViewModel.cs
@@ -0,0 +1,63 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using System;
+using MvvmHelpers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WeatherAnalytics.View;
+using WeatherDataParser.CLASSES;
+using WeatherAnalitycs.Utility;
+using WeatherDataParser;
+
+namespace WeatherAnalitycs.ViewModel
+{
+    internal class MainViewModel : BaseViewModel
+    {
+        readonly Parser _parser = new();
+        public List<string> StationNames { get; set; }
+        
+        string _selectedStation;
+        public string SelectedStation { 
+            get { return _selectedStation; } 
+            set
+            {
+                _selectedStation = value;
+                SearchStore.StationId = int.Parse(_selectedStation.Split('(', ')')[1]);
+                OnPropertyChanged();
+            } 
+        }
+
+        public SearchParamsStore SearchStore { get; set; } = new();
+
+        public RelayCommand UpdateDataCommand { get; set; }
+        public RelayCommand AddNewStationCommand { get; set; }
+        public RelayCommand ExitCommand { get; set; }
+
+        public MainViewModel()
+        {
+            StationNames = _parser.GetStationNamesList();
+            SelectedStation = StationNames[0];
+            UpdateDataCommand = new RelayCommand(UpdateData);
+            AddNewStationCommand = new RelayCommand(AddNewStation);
+            ExitCommand = new RelayCommand(Exit);
+        }
+
+        void UpdateData()
+        {
+            DataUpdateWindow DUW = new();
+            DUW.ShowDialog();
+        }
+
+        void AddNewStation()
+        {
+            AddStationWindow ASW = new();
+            ASW.ShowDialog();
+        }
+        void Exit()
+        {
+            Environment.Exit(0);
+        }
+    }
+}
