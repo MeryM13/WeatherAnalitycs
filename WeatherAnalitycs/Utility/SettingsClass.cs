b0780c1b7237af75a555a07d847415baf81beece
MeryM13 <globa2003marya@gmail.com>
2024-11-12 12:01:02 +0700

    added bindings to settings window

--- WeatherAnalitycs/Utility/SettingsClass.cs
+++ WeatherAnalitycs/Utility/SettingsClass.cs
@@ -11,56 +11,58 @@ namespace WeatherAnalitycs.Utility
 {
     internal class SettingsClass
     {
-        string _settingsFilePath = "../../../settings.json"; 
+        string _settingsFilePath = "../../../settings.json";
 
         public DateTime StartingDate { get; set; } = DateTime.Parse("01.01.2015");
-        public string WindRoseDataType { get; set; } = "Percentage";
+        public string WindRoseDataType { get; set; } = "Проценты";
         public bool DifferentiateRoseByDefault { get; set; } = true;
         public string DatabaseServer { get; set; } = "SQLite";
+        public bool UseDefaultConnectionString { get; set; } = true;
         public string DatabaseConnectionString { get; set; } = "";
         public string ExcelSavePath { get; set; } = "";
         public bool DivideExcelTable { get; set; } = true;
         public DateInterval ExcelTableDivider { get; set; } = DateInterval.Year;
 
-        public async void SaveToFile()
+        public async Task SaveToFile()
         {
             using (FileStream fs = new(_settingsFilePath, FileMode.OpenOrCreate))
             {
                 await JsonSerializer.SerializeAsync(fs, this);
-                fs.Close();
             }
         }
 
-        public async void LoadFromFile() 
+        public async Task LoadFromFile()
         {
             if (!File.Exists(_settingsFilePath))
-                SaveToFile();
+                await SaveToFile();
 
             using (FileStream fs = new(_settingsFilePath, FileMode.Open))
             {
-                SettingsClass settings = await JsonSerializer.DeserializeAsync<SettingsClass>(fs);
-                StartingDate = settings.StartingDate;
-                WindRoseDataType = settings.WindRoseDataType;
-                DifferentiateRoseByDefault = settings.DifferentiateRoseByDefault;
-                DatabaseServer = settings.DatabaseServer;
-                DatabaseConnectionString = settings.DatabaseConnectionString;
-                ExcelSavePath = settings.ExcelSavePath;
-                DivideExcelTable = settings.DivideExcelTable;
-                ExcelTableDivider = settings.ExcelTableDivider;
-                fs.Close();
+                try
+                {
+                    SettingsClass settings = await JsonSerializer.DeserializeAsync<SettingsClass>(fs);
+                    StartingDate = settings.StartingDate;
+                    WindRoseDataType = settings.WindRoseDataType;
+                    DifferentiateRoseByDefault = settings.DifferentiateRoseByDefault;
+                    DatabaseServer = settings.DatabaseServer;
+                    DatabaseConnectionString = settings.DatabaseConnectionString;
+                    ExcelSavePath = settings.ExcelSavePath;
+                    DivideExcelTable = settings.DivideExcelTable;
+                    ExcelTableDivider = settings.ExcelTableDivider;
+                }
+                catch (Exception ex)
+                {
+                }
             }
         }
 
-        public SettingsClass() 
+        public SettingsClass()
         {
-            //SaveToFile();
-            LoadFromFile();
         }
 
         public SettingsClass(string path)
         {
             _settingsFilePath = path;
-            LoadFromFile();
         }
     }
 }
--- WeatherAnalitycs/ViewModel/SettingsViewModel.cs
+++ WeatherAnalitycs/ViewModel/SettingsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using Microsoft.VisualBasic;
 using Microsoft.Win32;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using MvvmHelpers;
@@ -8,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Navigation;
 using WeatherAnalitycs.Utility;
 
 namespace WeatherAnalitycs.ViewModel
@@ -17,6 +19,15 @@ namespace WeatherAnalitycs.ViewModel
         Window _window;
         SettingsClass _settingsClass = new();
 
+        readonly Dictionary<string, DateInterval> intervalDict = new()
+        {
+            {"День", DateInterval.Day },
+            {"Неделя", DateInterval.WeekOfYear },
+            {"Месяц", DateInterval.Month },
+            {"Квартал", DateInterval.Quarter },
+            {"Год", DateInterval.Year },
+        };
+
         public SettingsClass Settings
         {
             get => _settingsClass;
@@ -24,6 +35,19 @@ namespace WeatherAnalitycs.ViewModel
             {
                 _settingsClass = value;
                 OnPropertyChanged(nameof(Settings));
+            }
+        }
+
+        public List<string> DatabasesNames { get; set; }
+        public List<string> WindRoseDataTypes { get; set; }
+        public List<string> Intervals { get => intervalDict.Select(x => x.Key).ToList(); }
+        public string IntervalName
+        {
+            get => intervalDict.FirstOrDefault(x => x.Value == Settings.ExcelTableDivider).Key;
+            set
+            {
+                Settings.ExcelTableDivider = intervalDict[value];
+                OnPropertyChanged(nameof(IntervalName));
             }
         }
 
@@ -36,6 +60,7 @@ namespace WeatherAnalitycs.ViewModel
         public SettingsViewModel(Window window)
         {
             _window = window;
+            Load();
             SaveSettingsCommand = new RelayCommand(SaveSettings);
             ExitCommand = new RelayCommand(Exit);
             ResetDatabaseCommand = new RelayCommand(ResetDatabase);
@@ -43,9 +68,16 @@ namespace WeatherAnalitycs.ViewModel
             OpenFileExplorerForExcelPathCommand = new RelayCommand(OpenFileExplorerForExcelPath);
         }
 
-        void SaveSettings()
+        async void Load()
         {
-            _settingsClass.SaveToFile();
+            DatabasesNames = ["SQLite", "MSSQL"];
+            WindRoseDataTypes = ["Проценты", "Числа"];
+            await _settingsClass.LoadFromFile();
+        }
+
+        async void SaveSettings()
+        {
+            await _settingsClass.SaveToFile();
             _window.Close();
         }
 
